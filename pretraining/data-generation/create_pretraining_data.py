4ef0ed87ca46cef6ac92b9c766bba30b158b043d
Marek Żochowski <marek@zochowski.net>
2020-07-04 13:11:54 +0200

    Fix tokenize call

--- pretraining/data-generation/create_pretraining_data.py
+++ pretraining/data-generation/create_pretraining_data.py
@@ -186,7 +186,7 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
         # Empty lines are used as document delimiters
         if not line:
           all_documents.append([])
-        tokens = tokenizer.tokenize(line)
+        tokens = tokenizer.tokenize(line, max_seq_length)
         if tokens:
           all_documents[-1].append(tokens)
         bar.add(1)
